a42ef632d0dc6da96c1f42da9bfe1a51e7f235c3
Jason <jajones414@gmail.com>
2023-02-06 19:37:23 -0600

    Set up starter code, created react app, set up mongoose connection.

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const express = require('express')
+const connection = require('./config/connection')
+
+const PORT = process.env.PORT || 3001
+const app = express()
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '..', 'client', 'build')))
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
+})
+
+connection.once('open', async () => {
+  app.listen(PORT, () => {
+    console.log(`Express server listening on http://localhost:${PORT}`)
+  })
+})
